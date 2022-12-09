d7f007f9a8992ea03a463c1cb6f3332a5c4beeb0
Aidil.chairi <62952600+aidilchw@users.noreply.github.com>
2022-12-09 23:54:43 +0700

    Add files via upload

---
+++ tugas KK js/kf.js
@@ -0,0 +1,17 @@
+function fizzBuzz(angka) {
+
+    for (var i = 1; i <= angka; i++) {
+  
+      if (i % 15 === 0) console.log("FizzBuzz");
+  
+      else if (i % 3 === 0) console.log("Fizz");
+  
+      else if (i % 5 === 0) console.log("Buzz");
+       
+      else console.log(i);
+  
+    }
+  
+  }
+  
+  fizzBuzz(15);
